5901baf0d2eccdc94266bd92f27ca5a624e1cb49
Daniel Lucas Costa Rodrigues <daniel.l.c.rodrigues@gmail.com>
2025-06-29 23:49:25 -0300

    Fix: error in api

--- config/config.go
+++ config/config.go
@@ -1,9 +1,7 @@
 package config
 
 import (
-	"fmt"
 	"log"
-	"os"
 
 	"github.com/vaizerds/gotasker-api/models"
 	"gorm.io/driver/postgres"
@@ -13,30 +11,45 @@ import (
 var DB *gorm.DB
 
 func ConnectDB() {
-	dsn := fmt.Sprintf(
-		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
-		getEnv("DB_HOST", "localhost"),
-		getEnv("DB_USER", "postgres"),
-		getEnv("DB_PASSWORD", "postgres"),
-		getEnv("DB_NAME", "gotasker_db"),
-		getEnv("DB_PORT", "5432"),
-	)
-
+	dsn := "host=localhost user=postgres password=postgres dbname=gotasker_db port=5432 sslmode=disable"
 	database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatalf("Failed to connect to database: %v", err)
 	}
 
 	DB = database
+
 	err = DB.AutoMigrate(&models.User{}, &models.Task{})
 	if err != nil {
 		log.Fatalf("Failed to migrate database: %v", err)
 	}
 }
 
-func getEnv(key, defaultValue string) string {
-	if val, exists := os.LookupEnv(key); exists {
-		return val
-	}
-	return defaultValue
-}
+// func ConnectDB() {
+// 	dsn := fmt.Sprintf(
+// 		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+// 		getEnv("DB_HOST", "localhost"),
+// 		getEnv("DB_USER", "postgres"),
+// 		getEnv("DB_PASSWORD", "postgres"),
+// 		getEnv("DB_NAME", "gotasker_db"),
+// 		getEnv("DB_PORT", "5432"),
+// 	)
+
+// 	database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+// 	if err != nil {
+// 		log.Fatalf("Failed to connect to database: %v", err)
+// 	}
+
+// 	DB = database
+// 	err = DB.AutoMigrate(&models.User{}, &models.Task{})
+// 	if err != nil {
+// 		log.Fatalf("Failed to migrate database: %v", err)
+// 	}
+// }
+
+// func getEnv(key, defaultValue string) string {
+// 	if val, exists := os.LookupEnv(key); exists {
+// 		return val
+// 	}
+// 	return defaultValue
+// }
