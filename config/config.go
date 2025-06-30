4e1349be0fe0942e0ac2d3d80b4d06a1ac907667
Daniel Lucas Costa Rodrigues <daniel.l.c.rodrigues@gmail.com>
2025-06-29 23:16:32 -0300

    First Commit

--- config/config.go
+++ config/config.go
@@ -23,26 +23,73 @@
 // 	}
 // }
 
+// package config
+
+// import (
+// 	"log"
+
+// 	"github.com/glebarez/sqlite"
+// 	"github.com/vaizerds/gotasker-api/models"
+// 	"gorm.io/gorm"
+// )
+
+// var DB *gorm.DB
+
+// func ConnectDB() {
+// 	database, err := gorm.Open(sqlite.Open("gotasker.db"), &gorm.Config{})
+// 	if err != nil {
+// 		log.Fatalf("Failed to connect to database: %v", err)
+// 	}
+// 	DB = database
+
+// 	if err := DB.AutoMigrate(&models.User{}, &models.Task{}); err != nil {
+// 		log.Fatalf("AutoMigrate failed: %v", err)
+// 	}
+// }
+
 package config
 
 import (
+	"fmt"
 	"log"
+	"os"
 
-	"github.com/glebarez/sqlite"
 	"github.com/vaizerds/gotasker-api/models"
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 var DB *gorm.DB
 
 func ConnectDB() {
-	database, err := gorm.Open(sqlite.Open("gotasker.db"), &gorm.Config{})
+	// Exemplo de conexão (ajuste conforme seu ambiente)
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+		getEnv("DB_HOST", "localhost"),
+		getEnv("DB_USER", "postgres"),
+		getEnv("DB_PASSWORD", "postgres"),
+		getEnv("DB_NAME", "gotasker_db"),
+		getEnv("DB_PORT", "5432"),
+	)
+
+	database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatalf("Failed to connect to database: %v", err)
 	}
+
 	DB = database
 
-	if err := DB.AutoMigrate(&models.User{}, &models.Task{}); err != nil {
-		log.Fatalf("AutoMigrate failed: %v", err)
+	// AutoMigrate models
+	err = DB.AutoMigrate(&models.User{}, &models.Task{})
+	if err != nil {
+		log.Fatalf("Failed to migrate database: %v", err)
 	}
+}
+
+// Utilitário para ler variáveis de ambiente com fallback
+func getEnv(key, defaultValue string) string {
+	if value, exists := os.LookupEnv(key); exists {
+		return value
+	}
+	return defaultValue
 }
--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.22
 
 require (
 	github.com/gin-gonic/gin v1.10.0
-	github.com/glebarez/sqlite v1.4.5
+	gorm.io/driver/postgres v1.4.5
 	gorm.io/gorm v1.30.0
 )
 
@@ -15,12 +15,10 @@ require (
 	github.com/cloudwego/iasm v0.2.0 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.3 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
-	github.com/glebarez/go-sqlite v1.18.2 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.20.0 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
-	github.com/google/uuid v1.3.0 // indirect
 	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
 	github.com/jackc/pgconn v1.13.0 // indirect
 	github.com/jackc/pgio v1.0.0 // indirect
@@ -38,7 +36,6 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.2 // indirect
-	github.com/remyoudompheng/bigfft v0.0.0-20200410134404-eec4a21b6bb0 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.12 // indirect
 	golang.org/x/arch v0.8.0 // indirect
@@ -48,9 +45,4 @@ require (
 	golang.org/x/text v0.20.0 // indirect
 	google.golang.org/protobuf v1.34.1 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
-	gorm.io/driver/postgres v1.4.5 // indirect
-	modernc.org/libc v1.18.0 // indirect
-	modernc.org/mathutil v1.5.0 // indirect
-	modernc.org/memory v1.3.0 // indirect
-	modernc.org/sqlite v1.18.2 // indirect
 )
